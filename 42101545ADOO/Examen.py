c2d2e37cd42382fea11d72ef7f79233e0c8b1d46
BlackPonhg <ricardogarciacabral@gmail.com>
2023-05-26 03:22:56 -0600

    Tareas ADOO

--- 42101545ADOO/Examen.py
+++
@@ -1,6 +0,0 @@
-class Examen:
-    def __init__(self, clave: int, nombre: str, aciertos: int, activo: bool):
-        self.clave = clave
-        self.nombre = nombre
-        self.aciertos = aciertos
-        self.activo = activo
--- 42101545ADOO/Profesor.py
+++
@@ -1,6 +0,0 @@
-class Profesor:
-    def __init__(self, rfc: str, nombre: str, apaterno: str, amaterno: str):
-        self.rfc = rfc
-        self.nombre = nombre
-        self.apaterno = apaterno
-        self.amaterno = amaterno
